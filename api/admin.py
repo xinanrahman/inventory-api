7361dae3122b1900ca6b8b0feed6d1f7b3400664
Xinan Rahman <40236985+xinanrahman@users.noreply.github.com>
2021-08-03 19:03:54 +0600

    add search filtering

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from . import models 
 
-# Register your models here.
-admin.site.register(models.Item)
+admin.site.site_header = "Custom"
 
+# Register your models here.
+@admin.register(models.Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["title", "stock_quantiy", "rating", "price", "in_stock"]
+    list_editable = ["stock_quantiy", "price", "in_stock"]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.filters import SearchFilter
+from rest_framework import status 
+from . import serializers
+from . import models 
 
 # Create your views here.
+# class ItemAPIView(APIView):
+#     def get(self, request):
+#         items = models.Item.objects.all()
+#         serializer = serializers.ItemSerializer(items, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+#     def post(self, request):
+#         serializer = serializers.ItemSerializer(data=request.data)
+#         print(request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({
+#                 "message": "item saved successfully"
+#             })
+
+#         return Response(serializer.errors)
+
+class ItemAPIView(ListCreateAPIView):
+    serializer_class = serializers.ItemSerializer
+    queryset = models.Item.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
